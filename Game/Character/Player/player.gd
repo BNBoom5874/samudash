8ad9884de47be8d3189dab74befcdd5d7945626c
BNBoom <bumboom86@gmail.com>
2026-05-29 00:08:21 +0700

    Player

--- Game/Character/Player/player.gd
+++ Game/Character/Player/player.gd
@@ -5,9 +5,20 @@ const DashSpeed : float = 1200.0
 const Gravity : float = 600.0
 
 var dashspeed  = DashSpeed
-
 var gravity = Gravity
 
+#time
+const Cooldown : float = 0.4
+const Time_Dash : float = 0.1
+
+
+var timer_dash : float = 0.0
+var timercool : float = 0.0
+
+
+var is_dashing : bool = false
+var is_cooldown : bool = false
+var wall_hit : bool = false
 
 #input
 var keyUp :bool = false
@@ -20,21 +31,27 @@ var keyDownL :bool = false
 var keyDownR :bool = false
 var keyDown :bool = false
 
+#region Loop function 
+
 func _ready() -> void:
 	pass
 
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	
+	set_Time(delta)
 	
 	get_input()
 	
-	Dash()
+	handle_Dash()
+	
 	
 	apply_gravity(delta)
 	
+	
 	move_and_slide()
 	
-	
+	handle_wall_collision()
 
 
 
@@ -49,21 +66,90 @@ func get_input() -> void:
 	keyDownR = Input.is_action_just_pressed("down_right")
 	keyDown = Input.is_action_just_pressed("down")
 
+#endregion
+
+#region Custom functions 
 
 func apply_gravity(delta) -> void:
-	if not is_on_floor():
+	if not is_on_floor() and not is_dashing:
 		velocity.y += gravity * delta
 
 
-func Dash() -> void:
-	#Up
-	if keyUp : velocity = Vector2(0,-1) * DashSpeed
-	if keyUpL : velocity = Vector2(-1, -1).normalized() * DashSpeed
-	if keyUpR : velocity = Vector2(1, -1).normalized() * DashSpeed
+func handle_Dash() -> void:
+	
+	if is_dashing or is_cooldown: return
+		#Up
+	if keyUp :
+		velocity = Vector2(0,-1) * DashSpeed
+	
+		
+	if keyUpL :
+		velocity = Vector2(-1, -1).normalized() * DashSpeed
+	
+		
+	if keyUpR :
+		velocity = Vector2(1, -1).normalized() * DashSpeed
+	
 	#horizon
-	if keyLeft :  velocity = Vector2(-1, 0) * DashSpeed
-	if keyRight : velocity = Vector2(1, 0) * DashSpeed
+	if keyLeft :
+		velocity = Vector2(-1, 0) * DashSpeed
+		
+	if keyRight :
+		velocity = Vector2(1, 0) * DashSpeed
+		
 	#Down
-	if keyDown : velocity = Vector2(0, 1) * DashSpeed
-	if keyDownL : velocity = Vector2(-1, 1).normalized() * DashSpeed
-	if keyDownR : velocity = Vector2(1, 1).normalized() * DashSpeed
+	if keyDown :
+		velocity = Vector2(0, 1) * DashSpeed
+		
+	if keyDownL :
+		velocity = Vector2(-1, 1).normalized() * DashSpeed
+		
+	if keyDownR :
+		velocity = Vector2(1, 1).normalized() * DashSpeed
+	
+	if keyUp or keyRight or keyLeft or keyDown or keyDownL or keyDownR or keyUpL or keyUpR:
+		is_dashing = true
+		timer_dash = Time_Dash
+	
+	
+	if Cooldown:
+		velocity.x = move_toward(velocity.x, 0, 20)
+
+
+func set_Time(delta) -> void:
+ 
+	
+	
+	if timer_dash > 0:
+		timer_dash -= delta
+	else:
+		timer_dash = 0.0
+		if is_dashing:
+			is_cooldown = true
+			timercool = Cooldown
+			is_dashing = false
+	
+	if timercool > 0:
+		timercool -= delta
+	else:
+		timercool = 0.0
+		if is_cooldown:
+			is_cooldown = false
+			wall_hit = false
+		
+		
+
+
+func handle_wall_collision() -> void:
+	if  is_dashing and is_on_wall() or is_on_ceiling():
+		velocity.x = 0
+		wall_hit = true
+		end_dash()
+
+
+func end_dash() -> void:
+	if wall_hit and not is_cooldown:
+		velocity = get_wall_normal() * 400
+		timercool = Cooldown
+		is_cooldown = true
+		is_dashing = false
